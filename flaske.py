f955e6e39b1f996934ca3f2619a3605c77159133
stenli <s.nevrovsky@ya.ru>
2023-08-08 15:30:55 +0300

    Add flask

--- flaske.py
+++ flaske.py
@@ -2,8 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Sample data array
+data = [
+    {
+        "db": "Database 1",
+        "shed": [
+            {"sh": "Collection 1", "freq": "Never"},
+            {"sh": "Collection 2", "freq": "Once a day"},
+            {"sh": "Collection 3", "freq": "Once a week"}
+        ]
+    },
+    {
+        "db": "Database 2",
+        "shed": [
+            {"sh": "Collection 4", "freq": "Once a day"},
+            {"sh": "Collection 5", "freq": "Never"},
+            {"sh": "Collection 6", "freq": "Once a week"}
+        ]
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
